fb666948dc80da46b19d39a47d23202109b7ae0d
walter <debugger2014@gmail.com>
2020-07-26 15:39:02 +0800

    update: github link

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <div class="vh-100 mx-3">
 
       <div class="h2 text-info text-center py-5">
-        The Data Copied from Google Excel or CSV to generate the template code.
+        The Excel or CSV Data to any data structure that you want.
       </div>
       <b-container fluid>
 
@@ -49,7 +49,7 @@
                         v-model="selectedExampleValue"
                         @change="examplesChange"
                         style="width:12rem">
-                        </b-select>
+              </b-select>
               <b-button type="button"
                         class="ml-3"
                         @click="createExampleByheaders">Create Example by headers</b-button>
@@ -73,9 +73,23 @@
 
       </b-container>
 
-      <div class="h1 secondary text-center py-5">
-        power by Walter
-      </div>
+      <a class="d-flex justify-content-center align-items-center h1 secondary py-5 text-dark"
+         href="https://github.com/walter-la/DataTemplatePlaygoundCode"
+         target="_blank"
+         rel="noopener noreferrer">
+        <span class="mx-3">power by</span>
+        <svg class="octicon octicon-mark-github v-align-middle"
+             height="32"
+             viewBox="0 0 16 16"
+             version="1.1"
+             width="32"
+             aria-hidden="true">
+          <path fill-rule="evenodd"
+                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+        </svg>
+        <span class="mx-3">walter-la</span>
+
+      </a>
     </div>
   </div>
 </template>
